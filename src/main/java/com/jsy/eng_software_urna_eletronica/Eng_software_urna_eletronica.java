44df819479f15991fc1222a406d7f9e7ecfef065
Joao-08 <joao.victorva08@gmail.com>
2024-12-02 20:12:10 -0300

    Added function create user

--- src/main/java/com/jsy/eng_software_urna_eletronica/Eng_software_urna_eletronica.java
+++ src/main/java/com/jsy/eng_software_urna_eletronica/Eng_software_urna_eletronica.java
@@ -6,6 +6,7 @@ package com.jsy.eng_software_urna_eletronica;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import com.jsy.eng_software_urna_eletronica.controllers.Database;
 import com.jsy.eng_software_urna_eletronica.controllers.User;
@@ -19,6 +20,85 @@ public class Eng_software_urna_eletronica {
 
     public static void main(String[] args) throws SQLException {
     	Database db = new Database();
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int c0 = -1;
+    	
+    	do {
+    		System.out.println("-------------------- MENU --------------------");
+    		System.out.println("1. Cadastrar Pessoa");
+    		System.out.println("2. Remover Pessoa");
+    		System.out.println("3. Editar Pessoa");
+    		System.out.println("4. Iniciar Votação");
+    		System.out.println("5. Relatório de uma Votação");
+    		System.out.println("6. Sair");
+    		System.out.println("----------------------------------------------");
+    		c0 = sc.nextInt();
+    		
+    		
+    		switch(c0) {
+    		case 1:
+    			System.out.println("------------ CADASTRO ------------");
+    	        System.out.println("Informe o nome do usuário: ");
+    	        String nome = sc.nextLine();
+    	        
+    	        System.out.println("Informe o cargo da pessoa: ");
+    	        System.out.println("1. CIVIL");
+    	        System.out.println("2. SENADOR");
+    	        System.out.println("3. DEPUTADO");
+    	        System.out.println("4. GOVERNADOR");
+    	        System.out.println("5. PRESIDENTE");
+    	        int cCargo = sc.nextInt();
+    	        sc.nextLine(); // Consome a quebra de linha
+    	        
+    	        int[] votaveis = new int[]{2, 3, 4, 5};
+    	        boolean isVotavel = false;
+    	        String Cargo = "CIVIL";
+    	        for (int cargo : votaveis) {
+    	            if (cargo == cCargo) {
+    	                isVotavel = true;
+    	                
+    	                switch(cCargo) {
+    	                case 2:
+    	                	Cargo = "SENADOR";
+    	                	break;
+    	                case 3:
+    	                	Cargo = "DEPUTADO";
+    	                	break;
+    	                case 4:
+    	                	Cargo = "GOVERNADOR";
+    	                	break;
+    	                case 5:
+    	                	Cargo = "PRESIDENTE";
+    	                	break;
+    	                }
+    	                break;
+    	            }
+    	        }
+
+    	        if (isVotavel) {
+    	            System.out.println("Informe o partido do usuário");
+    	            String partido = sc.nextLine();
+    	            User user = new User(0, nome, partido,Cargo);
+    	        } else {
+    	            User user = new User(0, nome, null, "CIVIL");
+    	        }
+    			
+    			break;
+    		case 2:
+    			
+        		break;
+    		case 3:
+    			
+        		break;
+    		case 4:
+    			
+        		break;
+    		case 5:
+    			
+        		break;
+    		}
+    	}while(c0 != 6 ); // Close Menu
 //    	Database.seedDatabase(500);
     	
     	User teste = new User(0,"Jhony","PTsOL","PRESIDENTE");
